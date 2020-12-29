ca2e3134326c658b83660d036da579c0beb7dc82
josephbyr <josephbyrne217@gmail.com>
2020-12-29 17:41:19 +0000

    added Board, Move, and Colour classes

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,5 +1,21 @@
 package com.chess.engine.pieces;
 
-public class Piece {
+import com.chess.engine.Colour;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+
+import java.util.List;
+
+public abstract class Piece {
+    protected final int piecePosition;
+    protected final Colour pieceColour;
+
+    Piece(final int piecePosition, final Colour pieceColour){
+        this.piecePosition = piecePosition;
+        this.pieceColour = pieceColour;
+    }
+
+    public abstract List<Move>calcLegalMoves(final Board board);
+
     
 }
