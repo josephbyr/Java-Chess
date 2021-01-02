4b0e342b5649d71ce9aa319ce564226c84f7018c
josephbyr <josephbyrne217@gmail.com>
2021-01-02 16:44:15 +0000

    more knight + new BoardUtils class

--- src/com/chess/engine/pieces/Knight.java
+++ src/com/chess/engine/pieces/Knight.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.chess.engine.Colour;
 import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
 import com.chess.engine.board.Tile;
 import com.google.common.collect.ImmutableList;
@@ -19,16 +20,22 @@ public class Knight extends Piece{
 
     @Override
     public List<Move> calcLegalMoves(Board board){
-        
-        int candidateDestinationCoordinate;
+
         final List<Move> legalMoves = new ArrayList<>();
 
-        for(int currentCandidate : CANDIDATE_MOVE_COORDINATES){
-            candidateDestinationCoordinate = this.piecePosition + currentCandidate;
+        for(int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES){
+            final int candidateDestinationCoordinate = this.piecePosition + currentCandidateOffset;
         
-            // if valid tile coordinate TO DO
-            if(true){
+            // if valid tile coordinate
+            if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
                 final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+
+                if(isFirstColumnException(this.piecePosition, currentCandidateOffset) || 
+                        isSecondColumnException(this.piecePosition, currentCandidateOffset) || 
+                        isSeventhColumnException(this.piecePosition, currentCandidateOffset) || 
+                        isEighthColumnException(this.piecePosition, currentCandidateOffset)){
+                    continue;
+                }
 
                 if(!candidateDestinationTile.isTileOccupied()){
                     legalMoves.add(new Move());
@@ -47,4 +54,22 @@ public class Knight extends Piece{
         return ImmutableList.copyOf(legalMoves);
     }
 
+    // Exceptions for knight movement
+    private static boolean isFirstColumnException(final int currentPos, final int candidateOffset){
+        return BoardUtils.FIRST_COLUMN[currentPos] && ((candidateOffset == -17) || (candidateOffset == -10) || 
+        (candidateOffset == 6) || (candidateOffset == 15));
+    }
+
+    private static boolean isSecondColumnException(final int currentPos, final int candidateOffset){
+        return BoardUtils.SECOND_COLUMN[currentPos] && ((candidateOffset == -10) || (candidateOffset == 6));
+    }
+
+    private static boolean isSeventhColumnException(final int currentPos, final int candidateOffset){
+        return BoardUtils.SEVENTH_COLUMN[currentPos] && ((candidateOffset == -6) || (candidateOffset == 10));
+    }
+
+    private static boolean isEighthColumnException(final int currentPos, final int candidateOffset){
+        return BoardUtils.EIGHTH_COLUMN[currentPos] && ((candidateOffset == -15) || (candidateOffset == -6) || 
+        (candidateOffset == 10) || (candidateOffset == 17));
+    }
 }
