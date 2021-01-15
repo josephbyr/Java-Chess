fd7dd6c0e7fa65882ccaf5a99ef2d7036878555d
josephbyr <josephbyrne217@gmail.com>
2021-01-15 15:31:33 +0000

    add Builder to Board

--- src/com/chess/engine/board/Board.java
+++ src/com/chess/engine/board/Board.java
@@ -1,7 +1,56 @@
 package com.chess.engine.board;
 
+import java.util.List;
+import java.util.Map;
+
+import com.chess.engine.Colour;
+import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableList;
+
 public class Board {
+
+    private final List<Tile> gameBoard;
+
+    private Board(Builder builder){
+        this.gameBoard = createGameBoard(builder);
+    }
+
     public Tile getTile(final int tileCoordinate){
         return null;
+    }
+
+    private static List<Tile> createGameBoard(Builder builder){
+        final Tile[] tiles = new Tile[BoardUtils.NUM_TILES];
+        for(int i = 0; i < BoardUtils.NUM_TILES; i++){
+            tiles[i] = Tile.createTile(i, builder.boardConfig.get(i));
+        }
+        return ImmutableList.copyOf(tiles);
+    }
+
+    public static Board createInitialBoard(){
+        return null;
+    }
+
+    public static class Builder{
+
+        Map<Integer, Piece> boardConfig;
+        Colour nextMoveMaker;
+
+        public Builder(){
+        }
+
+        public Builder setPiece(final Piece piece){
+            this.boardConfig.put(piece.getPiecePosition(), piece);
+            return this;
+        }
+
+        public Builder setMoveMaker(final Colour nextMoveMaker){
+            this.nextMoveMaker = nextMoveMaker;
+            return this;
+        }
+
+        public Board build(){
+            return new Board(this);
+        }
     }
 }
--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -18,6 +18,10 @@ public abstract class Piece {
         this.isFirstMove = false;
     }
 
+    public int getPiecePosition(){
+        return this.piecePosition;
+    }
+
     public Colour getPieceColour(){
         return this.pieceColour;
     }
