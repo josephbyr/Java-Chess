f1e0256b3327d0babf2ba6552fff5f5bc55173a9
josephbyr <josephbyrne217@gmail.com>
2021-04-02 20:14:11 +0000

    add execute() to MiniMax

--- src/com/chess/engine/player/ai/MiniMax.java
+++ src/com/chess/engine/player/ai/MiniMax.java
@@ -19,7 +19,37 @@ public class MiniMax implements MoveStrategy {
 
     @Override
     public Move execute(Board board, int depth) {
-        return null;
+        final long startTime = System.currentTimeMillis();
+        Move topMove = null;
+        int highestVal = Integer.MIN_VALUE;
+        int lowestVal = Integer.MAX_VALUE;
+        int currVal = 0;
+
+        System.out.println(board.currentPlayer() + "calculating with depth" + depth);
+
+        int numMoves = board.currentPlayer().getLegalMoves().size();
+
+        for(final Move move : board.currentPlayer().getLegalMoves()){
+            final MoveTransition moveTransition = board.currentPlayer().makeMove(move);
+            if(moveTransition.getMoveStatus().isDone()){
+                currVal = board.currentPlayer().getColour().isWhite() ?
+                    min(moveTransition.getTransitionBoard(), depth - 1) : 
+                    max(moveTransition.getTransitionBoard(), depth - 1);
+            }
+
+            if(board.currentPlayer().getColour().isWhite() && currVal >= highestVal){
+                highestVal = currVal;
+                topMove = move;
+            }
+            else if(board.currentPlayer().getColour().isBlack() && currVal <= lowestVal){
+                lowestVal = currVal;
+                topMove = move;
+            }
+        }
+
+        final long timeTaken = System.currentTimeMillis() - startTime;
+
+        return topMove;
     }
 
     public int min(final Board board, final int depth) {
