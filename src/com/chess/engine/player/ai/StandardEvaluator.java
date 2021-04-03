7947ff7c37f5afca7c5138f22d0919e4dc3a393d
josephbyr <josephbyrne217@gmail.com>
2021-04-03 20:01:31 +0000

    added more evaluation metrics

--- src/com/chess/engine/player/ai/StandardEvaluator.java
+++ src/com/chess/engine/player/ai/StandardEvaluator.java
@@ -4,21 +4,45 @@ import com.chess.engine.board.Board;
 import com.chess.engine.pieces.Piece;
 import com.chess.engine.player.Player;
 
-public class StandardEvaluator implements Evaluator{
+public class StandardEvaluator implements Evaluator {
+
+    private static final int CHECK_POINTS = 50;
+    private static final int CHECK_MATE_POINTS = Integer.MAX_VALUE;
+    private static final int DEPTH_POINTS = 100;
+    private static final int CASTLED_POINTS = 60;
 
     @Override
     public int evaluate(final Board board, final int depth) {
-        return evaluatePlayer(board, board.whitePlayer(), depth) - 
-               evaluatePlayer(board, board.blackPlayer(), depth);
+        return evaluatePlayer(board, board.whitePlayer(), depth) - evaluatePlayer(board, board.blackPlayer(), depth);
     }
 
     private int evaluatePlayer(final Board board, final Player player, final int depth) {
-        return pieceValue(player);
+        return pieceValue(player) + mobility(player) + check(player) + checkmate(player, depth) + castled(player);
+    }
+
+    private static int mobility(final Player player) {
+        return player.getLegalMoves().size();
+    }
+
+    private static int check(final Player player) {
+        return player.getOpponent().isInCheck() ? CHECK_POINTS : 0;
+    }
+
+    private static int checkmate(final Player player, int depth) {
+        return player.getOpponent().isInCheckMate() ? CHECK_MATE_POINTS * depthBonus(depth) : 0;
+    }
+
+    private static int depthBonus(int depth) {
+        return depth == 0 ? 1 : DEPTH_POINTS * depth;
+    }
+
+    private static int castled(final Player player) {
+        return player.isCastled() ? CASTLED_POINTS : 0;
     }
 
     private static int pieceValue(final Player player) {
         int pieceValueScore = 0;
-        for(final Piece piece : player.getActivePieces()){
+        for (final Piece piece : player.getActivePieces()) {
             pieceValueScore += piece.getPieceValue();
         }
         return pieceValueScore;
